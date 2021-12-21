9b0f459a11e6d492f41b58e7518c99ed7e50074a
Jung Gyu Yoon <yjg30737@gmail.com>
2021-12-21 09:11:31 +0900

    Apply renamed module, package name to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyqt-image-file-explorer',
+    name='pyqt-image-file-explorer-table-widget',
     version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    description='PyQt image file explorer',
-    url='https://github.com/yjg30737/pyqt-image-file-explorer.git',
+    description='PyQt QTableWidget for image file explorer',
+    url='https://github.com/yjg30737/pyqt-image-file-explorer-table-widget.git',
     install_requires=[
         'PyQt5>=5.8'
     ]
