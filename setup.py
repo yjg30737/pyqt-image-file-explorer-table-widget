c4bd9691b3b6958c8457b824ebd96a6e2602eab2
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-27 09:23:48 +0900

    Add new package (pyqt-resource-helper)

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     description='PyQt QTableWidget for image file explorer',
     url='https://github.com/yjg30737/pyqt-image-file-explorer-table-widget.git',
     install_requires=[
-        'PyQt5>=5.8'
+        'PyQt5>=5.8',
+        'pyqt-resource-helper @ git+https://git@github.com/yjg30737/pyqt-resource-helper.git@main'
     ]
 )
