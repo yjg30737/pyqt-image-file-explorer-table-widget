d4972da8d5378a628a4384e2b92109ae4b879290
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-13 08:23:35 +0900

    Update package's url, update setup.py to upload this to pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-image-file-explorer-table-widget',
-    version='0.2.2',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
@@ -10,8 +17,10 @@ setup(
     package_data={'pyqt_image_file_explorer_table_widget.style': ['image.css', 'table.css']},
     description='PyQt QTableWidget for image file explorer',
     url='https://github.com/yjg30737/pyqt-image-file-explorer-table-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
-        'pyqt-resource-helper @ git+https://git@github.com/yjg30737/pyqt-resource-helper.git@main'
+        'pyqt-resource-helper>=0.0.1'
     ]
 )
